249b81afa16d94473eec6a3adcc8a0436e6abcca
Rodion Martynov <rodion_martynov@epam.com>
2019-08-24 16:26:28 +0300

    Added loading and data splitting

---
+++ src/fraud/preprocessing.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import pandas as pd
+from sklearn.model_selection import TimeSeriesSplit
+import fire
+
+DATA_PATH = Path(__file__).parent.parent.parent / "data" / "creditcard.csv"
+TARGET = "Class"
+
+
+def load_data(path=DATA_PATH):
+    df = pd.read_csv(path)
+    return df
+
+
+def split_folds(in_path=DATA_PATH, out_dir=DATA_PATH.parent, n_splits=5):
+    # data
+    out_dir = Path(out_dir)
+    (out_dir / 'train').mkdir(parents=True, exist_ok=True)
+    (out_dir / 'test').mkdir(parents=True, exist_ok=True)
+    df = load_data(in_path)
+    y = df[TARGET]
+    tss = TimeSeriesSplit(n_splits=5)
+    for i, (train_idx, test_idx) in enumerate(tss.split(df, y)):
+        df.iloc[train_idx].to_csv(out_dir / "train" / f"fold_{i}.csv")
+        df.iloc[test_idx].to_csv(out_dir / "test" / f"fold_{i}.csv")
+
+
+if __name__ == "__main__":
+    fire.Fire()
